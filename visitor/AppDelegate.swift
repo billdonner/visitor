fe80671961d0dedd12aa72157358cd0068625802
bill donner <billdonner@blaptop.local>
2018-06-27 17:00:47 -0400

    shell app with upator as test harness, first commit

--- visitor/AppDelegate.swift
+++ visitor/AppDelegate.swift
@@ -8,14 +8,84 @@
 
 import UIKit
 
+class Piper {
+    var inputPipe: Pipe!
+    var outputPipe: Pipe!
+    var callback:((String)->())!
+    
+    @objc func handlePipeNotification(notification: Notification) {
+        //note you have to continuously call this when you get a message
+        //see this from documentation:
+        //Note that this method does not cause a continuous stream of notifications to be sent. If you wish to keep getting notified, you’ll also need to call readInBackgroundAndNotify() in your observer method.
+        inputPipe?.fileHandleForReading.readInBackgroundAndNotify()
+        
+        if let data = notification.userInfo![NSFileHandleNotificationDataItem] as? Data,
+            let str = String(data: data, encoding: String.Encoding.ascii) {
+            
+            //write the data back into the output pipe. the output pipe's write file descriptor points to STDOUT. this allows the logs to show up on the xcode console
+            outputPipe?.fileHandleForWriting.write(data)
+            
+            // `str` here is the log/contents of the print statement
+            //if you would like to route your print statements to the UI: make
+            //sure to subscribe to this notification in your VC and update the UITextView.
+            //Or if you wanted to send your print statements to the server, then
+            //you could do this in your notification handler in the app delegate.
+            
+            // presumably the callback will write to nsuserdefaults here
+            
+            self.callback(str)
+            
+            // do not print it will loop forever
+        }
+    }
+    
+    func openConsolePipe(_ callback:@escaping (String)->()) {
+        
+        self.callback = callback
+        //open a new Pipe to consume the messages on STDOUT and STDERR
+        inputPipe = Pipe()
+        
+        //open another Pipe to output messages back to STDOUT
+        outputPipe = Pipe()
+        
+        guard let inputPipe = inputPipe, let outputPipe = outputPipe else {
+            return
+        }
+        
+        let pipeReadHandle = inputPipe.fileHandleForReading
+        
+        //from documentation
+        //dup2() makes newfd (new file descriptor) be the copy of oldfd (old file descriptor), closing newfd first if necessary.
+        
+        //here we are copying the STDOUT file descriptor into our output pipe's file descriptor
+        //this is so we can write the strings back to STDOUT, so it can show up on the xcode console
+        dup2(STDOUT_FILENO, outputPipe.fileHandleForWriting.fileDescriptor)
+        
+        //In this case, the newFileDescriptor is the pipe's file descriptor and the old file descriptor is STDOUT_FILENO and STDERR_FILENO
+        
+        dup2(inputPipe.fileHandleForWriting.fileDescriptor, STDOUT_FILENO)
+        dup2(inputPipe.fileHandleForWriting.fileDescriptor, STDERR_FILENO)
+        
+        //listen in to the readHandle notification
+        NotificationCenter.default.addObserver(self, selector: #selector(self.handlePipeNotification), name: FileHandle.readCompletionNotification, object: pipeReadHandle)
+        
+        //state that you want to be notified of any data coming across the pipe
+        pipeReadHandle.readInBackgroundAndNotify()
+    }
+    
+
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    var piper: Piper?
+   
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // if not simulator, open console pipe
+
         return true
     }
 
--- visitor/ViewController.swift
+++ visitor/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var gameTimer: Timer!
+    
+    var counter = 0
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        gameTimer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(runTimedCode), userInfo: nil, repeats: true)
+        runTimedCode()
 
+    }
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+    @objc func runTimedCode () {
+        
+          counter += 1
+          self.topLabel.text = "\(self.counter)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("viewdidload")
+        #if !(targetEnvironment(simulator))
+        let piper = Piper()
+        piper.openConsolePipe(){ str in
+          
+            self.bottomLabel.text = str
+          
+        }
+        #endif
     }
 
     override func didReceiveMemoryWarning() {
